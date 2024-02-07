763163fc38f3d554ce57e34d9ab5ee2bc253bb39
Yuvrajput07 <129316979+Yuvrajput07@users.noreply.github.com>
2024-02-07 10:28:47 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,6 @@
+print(' ')
+print('Program to add two numbers')
+num1 = int(input(' Enter Any Number - '))
+num2 = int(input('Enter any Number - '))
+sum = num1+num2
+print('Sum of numbers is ',sum)
